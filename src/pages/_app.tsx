7e7138dba7d58b38e1adecfc1164e785531ec96f
Babur <saburovbabur@gmail.com>
2025-06-27 00:27:07 +0500

    generate meta tags for SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,16 +5,27 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import "@/styles/globals.css";
 import "@/styles/rich-text-editor.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
-    <TooltipProvider>
-      <NextIntlClientProvider locale={router.locale} timeZone="Asia/Tashkent" messages={pageProps.messages}>
-        <Component {...pageProps} />
-        <Toaster position="top-center" />
-      </NextIntlClientProvider>
-    </TooltipProvider>
+    <>
+      <Head>
+        <title>"Eсo Сarbon Services" MCHJ | ecocarbonservice</title>
+        <meta name="title" content='"Eсo Сarbon Services" MCHJ | ecocarbonservice' />
+        <meta
+          name="description"
+          content='"Eсo Сarbon Services" MCHJ O‘zbekiston gaz infratuzilmasida ekologik xavfsizlikni ta’minlash va iqlim o‘zgarishiga qarshi kurashish bo‘yicha yetakchi kompaniyalardan biridir. Biz xalqaro hamkorlik va ilg‘or texnologiyalar yordamida gaz sizib chiqishini aniqlash, tuzatish va emissiyalarni kamaytirish bo‘yicha samarali echimlar taklif qilamiz.'
+        />
+      </Head>
+      <TooltipProvider>
+        <NextIntlClientProvider locale={router.locale} timeZone="Asia/Tashkent" messages={pageProps.messages}>
+          <Component {...pageProps} />
+          <Toaster position="top-center" />
+        </NextIntlClientProvider>
+      </TooltipProvider>
+    </>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,29 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.ecocarbonservices.uz/" />
+        <meta property="og:title" content='"Eсo Сarbon Services" MCHJ | ecocarbonservice' />
+        <meta
+          property="og:description"
+          content='"Eсo Сarbon Services" MCHJ O‘zbekiston gaz infratuzilmasida ekologik xavfsizlikni ta’minlash va iqlim o‘zgarishiga qarshi kurashish bo‘yicha yetakchi kompaniyalardan biridir. Biz xalqaro hamkorlik va ilg‘or texnologiyalar yordamida gaz sizib chiqishini aniqlash, tuzatish va emissiyalarni kamaytirish bo‘yicha samarali echimlar taklif qilamiz.'
+        />
+        <meta property="og:image" content="https://ecocarbonservices.uz/eco-bg.png" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.ecocarbonservices.uz/" />
+        <meta property="twitter:title" content='"Eсo Сarbon Services" MCHJ | ecocarbonservice' />
+        <meta
+          property="twitter:description"
+          content='"Eсo Сarbon Services" MCHJ O‘zbekiston gaz infratuzilmasida ekologik xavfsizlikni ta’minlash va iqlim o‘zgarishiga qarshi kurashish bo‘yicha yetakchi kompaniyalardan biridir. Biz xalqaro hamkorlik va ilg‘or texnologiyalar yordamida gaz sizib chiqishini aniqlash, tuzatish va emissiyalarni kamaytirish bo‘yicha samarali echimlar taklif qilamiz.'
+        />
+        <meta property="twitter:image" content="https://ecocarbonservices.uz/eco-bg.png" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
